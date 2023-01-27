481c72932fe3439b7d5b9fa85d8e66ff4919c65a
Preesha <pg721@ic.ac.uk>
2023-01-27 11:05:09 +0000

    added function to main file to open and read test files

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -1,29 +1,49 @@
 package wacc
 
+import scala.io.Source
+
 import parsley.{Parsley, Success, Failure}
 import parsley.character.digit
 import parsley.expr.chain
 import parsley.implicits.character.charLift
+import parsley.combinator._
+
 
 object Main {
     def main(args: Array[String]): Unit = {
+
         println("Hello WACC_11!")
 
         lazy val integer = digit.foldLeft1[BigInt](0)((n, d) => n * 10 + d.asDigit)
 
         val add = (x: BigInt, y: BigInt) => x + y
         val sub = (x: BigInt, y: BigInt) => x - y
+        val mul = (x: BigInt, y: BigInt) => x * y
+        
+         div = (x: BigInt, y: BigInt) => x / y
 
         lazy val expr: Parsley[BigInt] =
             chain.left1[BigInt](
                 ('(' ~> expr <~ ')') <|> integer,
-                ('+' #> add) <|> ('-' #> sub)
+                ('+' #> add) <|> ('-' #> sub) <|> ('*' #> mul) <|> ('/' #> div)
             )
 
         expr.parse(args.head) match {
             case Success(x) => println(s"${args.head} = $x")
             case Failure(msg) => println(msg)
         }
+        
+    }
+
+    def openFile(file1: String) : Unit = {
+        val builder = new StringBuilder()
+        for (line <- Source.fromFile(args.head).getLines) {
+            builder.append(line)
+            builder.append(" ")
+        }
+        val result = builder.toString()
+        println(result)
     }
 }
+
 
