edca464621e067473047450d8f9743c24b5d15ad
Rishi Khiroya <rk1121@ic.ac.uk>
2023-01-29 17:15:09 +0000

    Modify main to open file given as arg.

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -4,26 +4,36 @@ import parsley.{Parsley, Success, Failure}
 import parsley.character.digit
 import parsley.expr.chain
 import parsley.implicits.character.charLift
+import scala.io.Source
 
 object Main {
     def main(args: Array[String]): Unit = {
         println("Hello WACC_11!")
 
-        lazy val integer = digit.foldLeft1[BigInt](0)((n, d) => n * 10 + d.asDigit)
-
-        val add = (x: BigInt, y: BigInt) => x + y
-        val sub = (x: BigInt, y: BigInt) => x - y
-
-        lazy val expr: Parsley[BigInt] =
-            chain.left1[BigInt](
-                ('(' ~> expr <~ ')') <|> integer,
-                ('+' #> add) <|> ('-' #> sub)
-            )
-
-        expr.parse(args.head) match {
-            case Success(x) => println(s"${args.head} = $x")
-            case Failure(msg) => println(msg)
+        if (args.length == 0) {
+            printUsage
+            return
         }
+
+        val filename = args(0)
+        val fileData = openFile(filename)
+        println(fileData) 
+        
     }
+
+    def printUsage : Unit = {
+        println("Incorrect usage.") // TODO: add usage message
+    }
+
+    def openFile(path: String) : String = {
+        val builder = new StringBuilder()
+        for (line <- Source.fromFile(path).getLines) {
+            builder.append(line)
+            builder.append("\n")
+        }
+        return builder.toString()
+        
+    }
+
 }
 
