eee38aefe50b99d19d1b0c6aa6c6b74357c32750
bc1221 <brett.conway21@imperial.ac.uk>
2023-01-30 21:18:24 +0000

    bc1221: implement basic parsing rules.

--- src/main/scala/wacc/Parser.scala
+++ src/main/scala/wacc/Parser.scala
@@ -12,7 +12,16 @@ object Parser{
     val PAIR_LIT = "null"
     val PAIR_ELEM = "fst" <|> "snd"
     val BASE_TYPE = "int" <|> "bool" <|> "char"  <|> "string"
-    val UNARY_OP = "!" <|> UNOP_MINUS <|> "len" <|> "ord" <|> "chr"
+    val UNARY_OP = "!" <|> UNOP_MINUS <|> "len" <|> "ord" <|> "chr"  // Not sure if these are tokenised already by lexer
+    val BINARY_OP = "*" <|> "/" <|>  "%" <|>  "+" <|>  "-" <|>  ">" <|>  ">=" <|>  "<" <|>  "<=" <|>  "==" <|>  "!=" <|>  "&&" <|> "||" // Not sure if these are tokenised already
+    val ARRAY_ELEM = IDENT *> "[" *> sepBy(EXPR, "][") <* "]" // Not sure about this one
+    val PAIR_TYPE = "pair" *> "(" *> sepBy1(PAIR_ELEM_TYPE, ",") <* ")"
+    val ARRAY_TYPE = (BASE_TYPE <|> PAIR_TYPE) <* "[]"           // should be TYPE but array makes it recursive
+    val TYPE = BASE_TYPE <|> ARRAY_TYPE <|> PAIR_TYPE
+
+    val PAIR_ELEM_TYPE = BASE_TYPE | "pair"  // needs ARRAY_TYPE too but is recursive??
+
+
     
     //lazy val EXPR: Parsley[Expr] = 
 
