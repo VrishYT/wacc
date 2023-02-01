eda9fd9cc052e4acc45b907d2aacfad3bb5bae92
Rishi Khiroya <rk1121@ic.ac.uk>
2023-02-01 10:33:44 +0000

    Implement control flow for checking expression type.
    Co-authored-by: Brett Conway <bc1221@ic.ac.uk>

--- src/main/scala/SemanticChecker.scala
+++ src/main/scala/SemanticChecker.scala
@@ -5,8 +5,9 @@ import AST._
 object SemanticChecker {
 
     import scala.collection.immutable.Map
+    import scala.collection.mutable.{Map => MapM}
 
-    def typecheck(program: Program) {
+    def typecheck(program: Program): Unit = {
         val statements = program.stats
         val functions = program.fs
 
@@ -16,13 +17,41 @@ object SemanticChecker {
 
     }
 
-    def checkFunction(func: Func) {
+    def checkFunction(func: Func): Unit = {
         val vars = func.args.map(param => (param.id -> param.t)).toMap
         checkStatements(func.stats, vars)
     }
 
-    def checkStatements(statements: List[Stat], vars: Map[String, Type]) {
-        ???
+    def checkStatements(statements: List[Stat], vars: Map[String, Type]): Unit = {
+        val childVars = MapM[String, Type]()
+        statements.foreach(statement => {
+            statement match {
+                case Declare(t, id, rhs) => ???
+                case Assign(x, y) => ???
+                case Read(x) => ??? 
+                case Free(x) => ??? 
+                case Return(x) => ??? 
+                case Exit(x) => checkExpressionType(x, IntType)
+                case Print(x, end) => ??? 
+                case If(p, x, y) => ??? 
+                case While(p, x) => ??? 
+                case Begin(xs) => checkStatements(xs, createChildVars(vars, childVars))
+            }
+        })
     }
+
+    def checkExpressionType(x: Expr, t: Type): Boolean = {
+        x match {
+            case IntLiteral(_) => ???
+            case CharLiteral(_) => ???
+            case StrLiteral(_) => ???
+            case BoolLiteral(_) => ???
+            case ArrayElem(_, _) => ???
+            case UnaryOpExpr(_, _) => ???
+            case BinaryOpExpr(_, _, _) => ???
+        }
+    }
+
+    def createChildVars(parent: Map[String, Type], child: MapM[String, Type]): Map[String, Type] = child.toMap ++ parent
     
 }
