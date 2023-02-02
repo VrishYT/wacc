e6d40ee69f005071be4308a2e10a3bc126b6049e
Rishi Khiroya <rk1121@ic.ac.uk>
2023-02-02 16:41:41 +0000

    Modify test suite to pass correct path to Compiler object
    Co-authored-by: Dami Elulade <dme21@ic.ac.uk>

--- src/test/scala/wacc/ParserTest.scala
+++ src/test/scala/wacc/ParserTest.scala
@@ -10,40 +10,52 @@ import org.scalatest.Ignore
 
 import java.nio.file.{Files, Paths}
 
-@Ignore
 class ValidParseTests extends AnyFunSuite {
 
     val examples = Paths.get("src/test/scala/wacc/wacc_examples/valid")
     Files.walk(examples).iterator().asScala.filter(_.getFileName.toString().endsWith(".wacc")).foreach(path => {
         test(path.getFileName.toString.replace(".wacc", "") + " are valid wacc files") {
-            val success: Boolean = Compiler(path.getFileName.toAbsolutePath.toString).parse
-            assert(success)
+            val c = Compiler(path.toString)
+            
+            val readSuccess = c.readTarget
+            assert(readSuccess)
+
+            val parseSuccess: Boolean = c.parse
+            assert(parseSuccess)
         }
     })
 
 }
 
-@Ignore
 class SemanticErrorParseTests extends AnyFunSuite {
 
     val examples = Paths.get("src/test/scala/wacc/wacc_examples/invalid/semanticErr")
     Files.walk(examples).iterator().asScala.filter(_.getFileName.toString().endsWith(".wacc")).foreach(path => {
         test(path.getFileName.toString.replace(".wacc", "") + " are semantically invalid") {
-            val success: Boolean = Compiler(path.getFileName.toAbsolutePath.toString).parse
-            assert(success)
+            val c = Compiler(path.toString)
+            
+            val readSuccess = c.readTarget
+            assert(readSuccess)
+
+            val parseSuccess: Boolean = c.parse
+            assert(parseSuccess)
         }
     })
 
 }
 
-@Ignore
 class SyntacticErrorParseTests extends AnyFunSuite {
 
     val examples = Paths.get("src/test/scala/wacc/wacc_examples/invalid/syntaxErr")
     Files.walk(examples).iterator().asScala.filter(_.getFileName.toString().endsWith(".wacc")).foreach(path => {
         test(path.getFileName.toString.replace(".wacc", "") + " have invalid syntax") {
-            val success: Boolean = Compiler(path.getFileName.toAbsolutePath.toString).parse
-            assert(!success)
+            val c = Compiler(path.toString)
+            
+            val readSuccess = c.readTarget
+            assert(readSuccess)
+
+            val parseSuccess: Boolean = c.parse
+            assert(!parseSuccess)
         }
     })
 
