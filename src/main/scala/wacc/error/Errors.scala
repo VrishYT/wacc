d1a3bf671f1de4434d831787b602490339aa0a98
Dami <dme21@ic.ac.uk>
2023-02-09 17:52:52 +0000

    Add line numbers to the line info output.

--- src/main/scala/wacc/error/Errors.scala
+++ src/main/scala/wacc/error/Errors.scala
@@ -22,18 +22,18 @@ object Errors {
 
     private val printPos: String = s"(line ${Integer.toUnsignedString(pos._1)}, column ${Integer.toUnsignedString(pos._2)})"
 
-    private val printLines: Seq[String] = error_lines.toSeqString()
+    private val printLines: Seq[String] = error_lines.toSeqString(pos)
 
     override def toString(): String = s"\n Syntax error ${source.fold("")(name => s"in $name ")}$printPos:\n${printLines.mkString("  ", "\n  ", "")}"
 
   }
 
   sealed trait WACCErrorLines {
-    def toSeqString(): Seq[String]
+    def toSeqString(pos: (Int, Int)): Seq[String]
   }
 
-  case class VanillaError(unexpected: Option[WACCErrorItem], expected: Seq[WACCErrorItem], reasons: Seq[String], lines: Seq[String]) extends WACCErrorLines {
-    override def toSeqString(): Seq[String] = {
+  case class VanillaError(unexpected: Option[WACCErrorItem], expected: Seq[WACCErrorItem], reasons: Seq[String], lines: WACCErrorInfo) extends WACCErrorLines {
+    override def toSeqString(pos: (Int, Int)): Seq[String] = {
       val unexpected_ = unexpected.map("unexpected " + _.value())
 
       val expected_ = combineAsList(expected.map(_.value()).toList)
@@ -41,12 +41,31 @@ object Errors {
       val reasons_ = reasons.collect {
         case reason if reason.nonEmpty => Some("- " + reason)
       }
-      combineOrUnknown((unexpected_ +: expected_ +: reasons_).flatten, lines)
+
+      combineOrUnknown((unexpected_ +: expected_ +: reasons_).flatten, lines.toSeqString(pos))
     }
   }
 
-  case class SpecialisedError(msgs: Seq[String], lines: Seq[String]) extends WACCErrorLines {
-   override def toSeqString(): Seq[String] = combineOrUnknown(msgs, lines)
+  case class SpecialisedError(msgs: Seq[String], lines: WACCErrorInfo) extends WACCErrorLines {
+    override def toSeqString(pos: (Int, Int)): Seq[String] = combineOrUnknown(msgs, lines.toSeqString(pos))
+  }
+
+  sealed trait WACCErrorInfo {
+    def toSeqString(pos: (Int, Int)): Seq[String]
+  }
+  
+  case class ParseErrorInfo(line: String, linesBefore: Seq[String], linesAfter: Seq[String], errorPointsAt: Int, errorWidth: Int, numLinesBefore: Int, numLinesAfter: Int) extends WACCErrorInfo {
+    override def toSeqString(pos: (Int, Int)): Seq[String] = {
+      val maxLength: Int = pos._1.toString.length + 1
+
+      Seq(s"") ++:
+      linesBefore.zipWithIndex.map(line => s"%${maxLength}d$errorLineStart${line._1}".format(pos._1 - numLinesBefore + {line._2})) ++:
+      Seq(s"%${maxLength}d$errorLineStart$line".format(pos._1), s"${" " * maxLength}$errorLineStart${errorPointer(errorPointsAt, errorWidth)}") ++:
+      linesAfter.zipWithIndex.map(line => s"%${maxLength}d$errorLineStart${line._1}".format(pos._1 + line._2 + 1))
+    }
+
+    private val errorLineStart = " | "
+    private def errorPointer(caretAt: Int, caretWidth: Int) = s"${(" " * caretAt)}${"^" * caretWidth}"
   }
 
   sealed trait WACCErrorItem {
