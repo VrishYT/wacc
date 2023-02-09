1f295d26555179166ac18a1daefe71652a28211a
bc1221 <brett.conway21@imperial.ac.uk>
2023-02-09 12:30:47 +0000

    Modify AST to take positional info
    Co-authored-by: Rishi Khiroya <rk1121@ic.ac.uk>

--- src/main/scala/wacc/ParserBridge.scala
+++ src/main/scala/wacc/ParserBridge.scala
@@ -6,25 +6,29 @@ import parsley.implicits.zipped.Zipped3
 
 object ParserBridge {
 
-    trait ParserBridgePos0[+A] {
+    trait ParserSingletonBridge[+A] {
         def con(pos: (Int, Int)): A
         def <#(op: Parsley[_]): Parsley[A] = pos.map(this.con(_)) <* op
     }
 
-    trait ParserBridgePos1[-A, +B] extends ParserBridgePos0[A => B]{
+    trait ParserBridgePos0[+A] extends ParserSingletonBridge[A] { this: A => 
+        override final def con(pos: (Int, Int)): A = this
+    }
+
+    trait ParserBridgePos1[-A, +B] extends ParserSingletonBridge[A => B] {
         def apply(x: A)(pos: (Int, Int)): B
         def apply(x: Parsley[A]): Parsley[B] = pos <**> x.map(this.apply(_) _)
         override final def con(pos: (Int, Int)): A => B = this.apply(_)(pos)
     }
 
-    trait ParserBridgePos2[-A, -B, +C] extends ParserBridgePos0[(A, B) => C]{
+    trait ParserBridgePos2[-A, -B, +C] extends ParserSingletonBridge[(A, B) => C] {
         def apply(x: A, y: B)(pos: (Int, Int)): C
         def apply(x: Parsley[A], y: Parsley[B]): Parsley[C] =
             pos <**> (x, y).zipped(this.apply(_, _) _)
         override final def con(pos: (Int, Int)): (A, B) => C = this.apply(_, _)(pos)
     }
 
-    trait ParserBridgePos3[-A, -B, -C, +D] extends ParserBridgePos0[(A, B, C) => D]{
+    trait ParserBridgePos3[-A, -B, -C, +D] extends ParserSingletonBridge[(A, B, C) => D] {
         def apply(x: A, y: B, z: C)(pos: (Int, Int)): D
         def apply(x: Parsley[A], y: Parsley[B], z: Parsley[C]) : Parsley[D]  =
             pos <**> (x, y, z).zipped(this.apply(_, _, _) _)
