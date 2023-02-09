fb0fb9edba5fc2ea5e8f16ef506d537d83f5804a
bc1221 <brett.conway21@imperial.ac.uk>
2023-02-09 14:56:01 +0000

    Overload error logger methods. Overload type exception methods.
    Co-authored-by: Rishi Khiroya <rk1121@ic.ac.uk>

--- src/main/scala/wacc/error/ErrorLogger.scala
+++ src/main/scala/wacc/error/ErrorLogger.scala
@@ -2,14 +2,16 @@ package wacc.error
 
 import parsley.Failure
 import parsley.position._
+import wacc.AST._
 
 object ErrorLogger {
 
     def err(failure: Failure[_]) = throw SyntaxException(failure)
 
-    def err(msg: String) = throw TypeException(msg)
-
-    def err(msg: String, pos: (Int, Int)*) = throw TypeException(msg, pos)
+    def err(msg: String) = throw TypeException(msg, None, Seq(), Seq())
+    def err(msg: String, pos: (Int, Int)*) = throw TypeException(msg, None, Seq(), pos)
+    def err(msg: String, actual: Type, expected: Type, pos: (Int, Int)*) = throw TypeException(msg, Some(actual), Seq(expected), pos)
+    def err(msg: String, actual: Type, expected: Seq[Type], pos: (Int, Int)*) = throw TypeException(msg, Some(actual), expected, pos)
 
     def err(msg: String, exit: Int) = exit match {
         case 100 => throw SyntaxException(msg)
--- src/main/scala/wacc/error/TypeException.scala
+++ src/main/scala/wacc/error/TypeException.scala
@@ -1,10 +1,18 @@
 package wacc.error
 
-final case class TypeException(private val message: String, private val pos: Seq[(Int, Int)]) extends CompilerException(message, 200) {
+import wacc.AST._
+
+final case class TypeException(private val message: String, 
+        private val actual: Option[Type], 
+        private val expected: Seq[Type], 
+        private val pos: Seq[(Int, Int)])
+    extends CompilerException(message, 200) {
     override def toString: String = "pos: " + pos + "\n" + message
 }
 
 object TypeException {
-    def apply(msg: String): TypeException = return TypeException(msg, Seq())
-    // def apply(msg: String, pos: (Int, Int)*): TypeException = return new TypeException(msg, pos)
+    def apply(msg: String): TypeException = TypeException(msg, None, Seq(), Seq())
+    def apply(msg: String, pos: Seq[(Int, Int)]): TypeException = TypeException(msg, None, Seq(), pos)
+    def apply(msg: String, actual: Type, expected: Type, pos: Seq[(Int, Int)]): TypeException = TypeException(msg, Some(actual), Seq(expected), pos)
+    def apply(msg: String, actual: Type, expected: Seq[Type], pos: Seq[(Int, Int)]): TypeException = TypeException(msg, Some(actual), expected, pos)
 }
