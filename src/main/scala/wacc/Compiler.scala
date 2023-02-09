dc1db09b56fd21657edd17b9c2486ced248b3e80
bc1221 <brett.conway21@imperial.ac.uk>
2023-02-09 23:11:02 +0000

    Fix Compiler import for WACCError type
    Co-authored-by: Rishi Khiroya <rk1121@ic.ac.uk>

--- src/main/scala/wacc/Compiler.scala
+++ src/main/scala/wacc/Compiler.scala
@@ -9,7 +9,7 @@ class Compiler(private val file: File) {
     import AST.Program
     import parsley.{Success, Failure}
     import error._
-    import error.Errors.WACCError
+    import Errors.WACCError
     import parsley.combinator.skipMany
     import parsley.character.whitespace
     import parsley.errors.{ErrorBuilder, Token, TokenSpan}
