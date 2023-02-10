a093b532a2a455ed21d926c8a647499a1c9535cb
Rishi <rk1121@ic.ac.uk>
2023-02-10 01:37:11 +0000

    Modify ErrorLogger err() methods to throw new TypeExceptions

--- src/main/scala/wacc/error/ErrorLogger.scala
+++ src/main/scala/wacc/error/ErrorLogger.scala
@@ -7,18 +7,13 @@ import Errors._
 
 object ErrorLogger {
 
-    def err(failure: Failure[WACCError]) = throw SyntaxException(failure)
-
-    def err(msg: String, exit: Int) = exit match {
-        case 100 => throw SyntaxException(msg)
-        case 200 => throw TypeException(msg)
-        case x => {
-            System.err.println(msg)
-            sys.exit(x)
-        }
+    def err(msg: String, exit: Int) = {
+        System.err.println(msg)
+        sys.exit(exit)
     }
-    def err(msg: String, pos: (Int, Int)*) = throw TypeException(msg, None, Seq(), pos)
-    def err(msg: String, actual: Type, expected: Type, pos: (Int, Int)*) = throw TypeException(msg, Some(actual), Seq(expected), pos)
-    def err(msg: String, actual: Type, expected: Seq[Type], pos: (Int, Int)*) = throw TypeException(msg, Some(actual), expected, pos)
+
+    def err(msg: String, pos: (Int, Int)*) = throw new TypeException(msg, None, pos)
+    def err(msg: String, actual: Type, expected: Type, pos: (Int, Int)*) = throw new TypeException(msg, Some(actual, Seq(expected)), pos)
+    def err(msg: String, actual: Type, expected: Seq[Type], pos: (Int, Int)*) = throw new TypeException(msg, Some(actual, expected), pos)
 
 }
