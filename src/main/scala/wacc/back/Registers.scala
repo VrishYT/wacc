b7d83161143e3bd091596d837833332e93688829
Rishi <rk1121@ic.ac.uk>
2023-02-13 16:06:41 +0000

    Add Register objects + general registers

--- src/main/scala/wacc/back/Registers.scala
+++ src/main/scala/wacc/back/Registers.scala
@@ -1,3 +1,8 @@
 package wacc.back
 
 sealed trait Register extends Operand
+case object SP extends Register /* Stack Pointer */
+case object LR extends Register /* Linked Register */
+case object PC extends Register /* Program Counter */
+case object FP extends Register /* Frame Pointer */
+case class Reg(i: Int) extends Register /* General Purpose Registers */
