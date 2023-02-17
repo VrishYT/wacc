3600f820370f79dcb9a5b1a055972feba25de82b
Rishi <rk1121@ic.ac.uk>
2023-02-17 18:03:31 +0000

    Refactor CodeGenerator to use RegisterAllocator
    Co-authored-by: Conway, Brett <brett.conway21@imperial.ac.uk>
    Co-authored-by: Gehlot, Preesha <preesha.gehlot21@imperial.ac.uk>

--- src/main/scala/wacc/back/CodeGenerator.scala
+++ src/main/scala/wacc/back/CodeGenerator.scala
@@ -1,23 +1,12 @@
 package wacc.back
 
 import wacc.AST._
-import scala.collection.LinearSeq
 
 object CodeGenerator {
 
     def generate(program: Program): Unit = {
-        val regs = LinearSeq(Reg(0),
-                       Reg(1), 
-                       Reg(2), 
-                       Reg(3), 
-                       Reg(4),
-                       Reg(5),
-                       Reg(6),
-                       Reg(7),
-                       Reg(8),
-                       Reg(9),
-                       Reg(10),
-                       Reg(11))
+
+        val regs = new RegisterAllocator
                        
         val ass = program.toAssembly(regs)
         ass.foreach(println)
