19537e6eb1b0b4fbc3d0b8bbb4352e0945d16194
Rishi <rk1121@ic.ac.uk>
2023-02-17 18:05:05 +0000

    Add Operands object with util opToReg function
    Co-authored-by: Conway, Brett <brett.conway21@imperial.ac.uk>
    Co-authored-by: Gehlot, Preesha <preesha.gehlot21@imperial.ac.uk>

--- src/main/scala/wacc/back/Operands.scala
+++ src/main/scala/wacc/back/Operands.scala
@@ -9,3 +9,13 @@ case class ImmChar(c: Char) extends Operand {
     def arm11: String = "#" + c.toInt
     override def toString(): String = arm11
 }
+
+object Operands {
+    def opToReg(op: Operand, regs: RegisterAllocator): (Register, Seq[Instruction]) = op match {
+        case x: Register => (x, Seq())
+        case _ => {
+            val reg = regs.allocate
+            (reg, Seq(Mov(reg, op)))
+        }
+    }
+}
