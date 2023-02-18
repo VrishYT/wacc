ef4aa97037a78d85de480a26f8b5a194c63bd45c
Rishi <rk1121@ic.ac.uk>
2023-02-18 16:47:39 +0000

    Modify symbol table to have overloaded add methods

--- src/main/scala/wacc/back/SymbolTable.scala
+++ src/main/scala/wacc/back/SymbolTable.scala
@@ -1,17 +1,21 @@
 package wacc.back
 
 import scala.collection.mutable.{Map => MapM}
+import wacc.AST._
 
 class SymbolTable {
 
     private val table = MapM[String, (Type, Option[String])]()
 
-    def add(id: String, t: Type) = add(id, t, None)
-    def add(id: String, t: Type, addr: String) = {
+    def add(id: String, t: Type): Boolean = add(id, t, None)
+    def add(id: String, t: Type, addr: String): Boolean = add(id, t, Some(addr))
+    def add(id: String, t: Type, addr: Option[String]): Boolean = {
         if (table.contains(id)) {
             // renaming 
+            return false
         } else {
-            table(id) = (t, Some(addr))
+            table(id) = (t, addr)
+            return true
         }
     }
 
