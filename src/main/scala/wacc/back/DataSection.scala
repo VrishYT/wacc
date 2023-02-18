4d1a8743b74d61d94be30619f5d7efdc5b5c8f3d
Preesha <pg721@ic.ac.uk>
2023-02-18 17:21:36 +0000

    Rougly implement data section object co-authored-by: Rishi Khiroya <rk1121@ic.ac.uk>

---
+++ src/main/scala/wacc/back/DataSection.scala
@@ -0,0 +1,22 @@
+package wacc.back
+
+import scala.collection.mutable.{Map => MapM}
+
+class DataSection {
+
+    private val table = MapM[String, String]()
+    private var counter = 0
+
+    def add(value: String): String = {
+        val label = ".L.str" + counter
+        table(label) = value
+        counter += 1
+        return label
+    }
+
+    def get(id: String): String = table.get(id) match {
+        case Some(x) => x
+        case None => ???
+    }
+    
+}
