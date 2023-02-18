e7b8bdfc479f459d248eba6b76b826e2bfe62e7e
Rishi <rk1121@ic.ac.uk>
2023-02-18 16:47:52 +0000

    Modify compiler class to have a symbol table instance

--- src/main/scala/wacc/Compiler.scala
+++ src/main/scala/wacc/Compiler.scala
@@ -2,7 +2,7 @@ package wacc
 
 import wacc.front.{Parser, SemanticChecker}
 import wacc.front.error.{ErrorLogger, TypeException, WACCError, WACCErrorBuilder}
-import wacc.back.CodeGenerator
+import wacc.back._
 
 import java.io.File
 
@@ -19,6 +19,7 @@ class Compiler(private val file: File) {
     import wacc.front.Lexing.{lexer, keywords}
 
     private var program: Option[Program] = None 
+    private val symbolTable = new SymbolTable
 
     def parse(): Boolean = {
         val pNode = Parser.program
@@ -57,7 +58,7 @@ class Compiler(private val file: File) {
     
     def typecheck: Boolean = program match {
         case Some(x) => {
-            val errors = SemanticChecker.typecheck(x)
+            val errors = SemanticChecker.typecheck(x, symbolTable)
             if (errors.isEmpty) return true
 
             TypeException.convertErrors(errors, file).foreach(println)
@@ -68,7 +69,7 @@ class Compiler(private val file: File) {
     }
 
     def compile: Unit = program match {
-        case Some(x) => CodeGenerator.generate(x)
+        case Some(x) => CodeGenerator.generate(x, symbolTable)
         case None => ErrorLogger.err("generate called before parse/typecheck", 1)
     }
 
