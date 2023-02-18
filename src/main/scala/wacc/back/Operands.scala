4e953126090bab4e659baf7725faff6f4ffcd665
Preesha <pg721@ic.ac.uk>
2023-02-18 17:23:01 +0000

    Implement labels in data section as operands. co-authored-by: Rishi Khiroya <rk1121@ic.ac.uk>

--- src/main/scala/wacc/back/Operands.scala
+++ src/main/scala/wacc/back/Operands.scala
@@ -1,12 +1,19 @@
 package wacc.back
 
 trait Operand
+
 case class ImmInt(i: Int) extends Operand {
     def arm11: String = "#" + i
     override def toString(): String = arm11
 }
+
 case class ImmChar(c: Char) extends Operand {
     def arm11: String = "#" + c.toInt
+    override def toString(): String = arm11
+}
+
+case class DataLabel(label: String) extends Operand {
+    def arm11: String = "=" + label
     override def toString(): String = arm11
 }
 
