7a77cb9fe7da48984e83b2fe8f89d5da2bf20d78
Rishi <rk1121@ic.ac.uk>
2023-02-20 01:04:29 +0000

    Add "\t" tabbing character to instructions' toString() methods where applicable.

--- src/main/scala/wacc/back/Instruction.scala
+++ src/main/scala/wacc/back/Instruction.scala
@@ -8,68 +8,68 @@ sealed trait Instruction
 /* Arithmetic Instructions */
 /* dest = src - op */
 case class Sub(dest: Register, src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"sub $dest, $src, $op"
+    override def toString(): String = s"\tsub $dest, $src, $op"
 }
 /* dest = src + op */
 case class Add(dest: Register, src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"add $dest, $src, $op"
+    override def toString(): String = s"\tadd $dest, $src, $op"
 }
 /* dest = src * op */
 case class Mul(dest: Register, src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"mul $dest, $src, $op"
+    override def toString(): String = s"\tmul $dest, $src, $op"
 }
 case class Div(dest: Register, src: Register, op: Operand) extends Instruction
 
 /* Compare Instructions */
 case class Cmp(src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"cmp $src, $op"
+    override def toString(): String = s"\tcmp $src, $op"
 }
 
 /* Logical Instructions */
 case class And(dest: Register, src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"and $dest, $src, $op"
+    override def toString(): String = s"\tand $dest, $src, $op"
 }
 case class Xor(dest: Register, src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"eor $dest, $src, $op"
+    override def toString(): String = s"\teor $dest, $src, $op"
 }
 case class Or(dest: Register, src: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"orr $dest, $src, $op"
+    override def toString(): String = s"\torr $dest, $src, $op"
 }
 
 case class Len(dest: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"mov $dest, $op"
+    override def toString(): String = s"\tmov $dest, $op"
 }
 
 /* Loading and storing Instructions */
 case class Mov(dest: Register, op: Operand) extends Instruction {
-    override def toString(): String = s"mov $dest, $op"
+    override def toString(): String = s"\tmov $dest, $op"
 }
 case class Load(dest: Register, src: Operand) extends Instruction {
-    override def toString(): String = s"ldr $dest, $src"
+    override def toString(): String = s"\tldr $dest, $src"
 }
 case class Store(dest: Register, src: Operand) extends Instruction {
-    override def toString(): String = s"str $dest, $src"
+    override def toString(): String = s"\tstr $dest, $src"
 }
 
 /* Branch Instructions */
 case class Branch(label: String, cond: Condition = AL) extends Instruction {
-    override def toString(): String = s"b${cond.toString.toLowerCase()} $label"
+    override def toString(): String = s"\tb${cond.toString.toLowerCase()} $label"
 }
 case class LinkBranch(label: String) extends Instruction {
-    override def toString(): String = s"bl $label"
+    override def toString(): String = s"\tbl $label"
 }
 
 /* Stack Instruction */
 case class Push(src: Register*) extends Instruction {
-    override def toString(): String = s"push {${src.mkString(", ")}}"
+    override def toString(): String = s"\tpush {${src.mkString(", ")}}"
 }
 case class Pop(dest: Register*) extends Instruction {
-    override def toString(): String = s"pop {${dest.mkString(", ")}}"
+    override def toString(): String = s"\tpop {${dest.mkString(", ")}}"
 }
 
 case class Label(label: String) extends Instruction {
     override def toString(): String = s"${label}:"
 }
-case class Directive(name: String) extends Instruction {
-    override def toString(): String = s"directive $name"
+case class Section(section: String) extends Instruction {
+    override def toString(): String = section
 }
