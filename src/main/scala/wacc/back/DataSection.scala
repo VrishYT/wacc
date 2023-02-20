600e69076ebeea4bc8ffc5c709e9be05bd07254b
Rishi <rk1121@ic.ac.uk>
2023-02-20 01:05:09 +0000

    Implement rough toString() for DataSection.

--- src/main/scala/wacc/back/DataSection.scala
+++ src/main/scala/wacc/back/DataSection.scala
@@ -1,9 +1,11 @@
 package wacc.back
 
 import scala.collection.mutable.{Map => MapM}
+import wacc.back._
 
 class DataSection {
 
+    // TODO: change key type to label ???
     private val table = MapM[String, String]()
     private var counter = 0
 
@@ -17,6 +19,24 @@ class DataSection {
     def get(id: String): String = table.get(id) match {
         case Some(x) => x
         case None => ???
+    }
+
+    override def toString(): String = {
+        if (table.isEmpty) return ""
+
+        // TODO: update according to other TODO's related to DataSection
+        def entryToAssembly(entry: (String, String)): String = {
+            val label = entry._1
+            val data = entry._2
+
+            return s"\t.word ${data.length()}\n" + 
+                   label + ":\n" +
+                   s"\t.asciz \"${data}\""
+        }
+
+        return ".data\n" + 
+               table.toSeq.map(entryToAssembly).mkString +
+               ".text\n"
     }
     
 }
