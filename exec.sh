46ba52ce1b5ce4228cf0acd62c48d733d478b57d
Rishi <rk1121@ic.ac.uk>
2023-02-21 12:19:00 +0000

    Add exec.sh script to run a .s file

---
+++ exec.sh
@@ -0,0 +1,8 @@
+ARG=$1
+BASEPATH=${ARG%.*}
+BASENAME=${BASEPATH##*/}
+arm-linux-gnueabi-gcc -o ${BASENAME} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "${BASENAME}.s"
+qemu-arm -L /usr/arm-linux-gnueabi/ ${BASENAME}
+echo "\n\n"
+echo $?
+rm ${BASENAME}
