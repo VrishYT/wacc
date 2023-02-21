4755f732fcd9ee21a0de02f3dd29e69f74f94b5b
Rishi <rk1121@ic.ac.uk>
2023-02-21 12:19:20 +0000

    Modify run.sh to add space between program output and exit code.

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ make
 ./compile ${ARG}
 arm-linux-gnueabi-gcc -o ${BASENAME} -mcpu=arm1176jzf-s -mtune=arm1176jzf-s "${BASENAME}.s"
 qemu-arm -L /usr/arm-linux-gnueabi/ ${BASENAME}
+echo "\n\n"
 echo $?
 rm ${BASENAME}
 rm "${BASENAME}.s"
