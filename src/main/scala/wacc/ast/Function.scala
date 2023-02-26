a2e8b91b3fe041f79e91220f24a54edcc2963ffc
Rishi <rk1121@ic.ac.uk>
2023-02-26 20:32:03 +0000

    Implement abstracted function generator. Modify Operands.opToReg to allocate an op to a specific reg

--- src/main/scala/wacc/ast/Function.scala
+++ src/main/scala/wacc/ast/Function.scala
@@ -16,7 +16,8 @@ case class Func(fs: (Type, String), args: List[Param], stats: List[Stat])(val po
         
         val statsOut = stats.map(_.toAssembly(gen)).fold(Seq())(_ ++ _)
 
-        return Seq(Label(s"wacc_${fs._2}"), Push(LR)) ++ statsOut :+ Pop(PC)
+        // return Seq(Label(s"wacc_${fs._2}"), Push(LR)) ++ instr :+ Pop(PC)
+        return Func.generateFunction(s"wacc_${fs._2}", instr)
     }
 
     /* define validReturn of a function, and match on the last statement : */
@@ -51,7 +52,47 @@ case class Func(fs: (Type, String), args: List[Param], stats: List[Stat])(val po
 }
 
 /* function and parameter companion objects with parser bridges */
-object Func extends ParserBridgePos3[(Type, String), List[Param], List[Stat], Func]
+object Func extends ParserBridgePos3[(Type, String), List[Param], List[Stat], Func] {
+
+    import scala.collection.mutable.{ListBuffer}
+
+    val Func_Regs = Seq(
+        Register(0),
+        Register(1),
+        Register(2),
+        Register(3),
+        Register(4),
+        Register(5),
+        Register(6),
+        Register(7),
+        Register(8),
+        Register(9),
+        Register(10)
+    )
+
+    def generateFunction(id: String, instr: Seq[Instruction]): Seq[Instruction] = Seq(
+        Label(id),
+        Push{LR}
+    ) ++ (instr :+ Pop{PC})
+
+    def callFunction(id: String, args: Seq[Operand] = Seq(), regsToSave: Seq[Register] = Seq()): Seq[Instruction] = {
+
+        if (args.length > Func_Regs.length) println("Too many args to load in") // TODO: load excess into mem 
+
+        val range = (0 until args.length.min(Func_Regs.length))
+
+        val regs = (range.map(Register(_)) ++ regsToSave).distinct
+        val push = Push(regs:_*)
+        val loadArgs = range.map(i => args(i) match {
+            case x: Register => Mov(Register(i), x)
+            case x => Operands.opToReg(x, Register(i)) 
+        })
+        val pop = Pop(regs:_*)
+
+        return push +: loadArgs :+ pop
+    }
+
+}
 
 /* parameter case class with position */
 case class Param(t: Type, id: String)(val pos: (Int, Int))
