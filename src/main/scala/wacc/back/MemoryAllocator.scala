c3672245e32ce7eddca698c0c7169daab7c9e94a
bc1221 <brett.conway21@imperial.ac.uk>
2023-02-26 21:06:20 +0000

    Refactor MemoryAllocator.
    Refactor mallocPair function.
    Add documentation to mallocPair.

--- src/main/scala/wacc/back/MemoryAllocator.scala
+++ src/main/scala/wacc/back/MemoryAllocator.scala
@@ -8,29 +8,50 @@ class MemoryAllocator {
     val elemSize = 4
 
     def mallocPair(fst: Operand, snd: Operand, out: Register): Assembly = {
+
+        val pop = Pop(Register(8))
+        val push = Push(Register(8))
+        val malloc = LinkBranch("malloc")
+
         def mallocElem(elem: Operand): Seq[Instruction] = {
             val instrs = Seq()
+
+            /* Malloc with elemSize parameter */
             instrs :+ Mov(Register(0), ImmInt(elemSize))
-            instrs :+ LinkBranch("malloc")
+            instrs :+ malloc
+
+            /* Move allocated mem address into out */
             instrs :+ Mov(out, Register(0))
+
+            /* Move elem into allocated address */
             instrs :+ Mov(Register(8), elem)
             instrs :+ Store(Register(8), Address(out, ImmInt(0)))
+
+            /* Push address of elem */
             instrs :+ Mov(Register(8), out)
-            instrs :+ Push(Register(8))
+            instrs :+ push
+
             return instrs
         }
+
         val instrs = mallocElem(fst) ++ mallocElem(snd)
 
+        /* Malloc with pair size parameter */
         instrs :+ Mov(Register(0), ImmInt(elemSize * 2))
-        instrs :+ LinkBranch("malloc")
-        instrs :+ Mov(out, Register(0))
-        instrs :+ Pop(Register(8))
-        instrs :+ Store(Register(8), Address(out, ImmInt(elemSize)))
-        instrs :+ Pop(Register(8))
-        instrs :+ Store(Register(8), Address(out, ImmInt(0)))
-        instrs :+ Mov(Register(8), out)
+        instrs :+ malloc
 
-        val assembly = Assembly(Register(8), instrs)
+        /* Move allocated mem address into out */
+        instrs :+ Mov(out, Register(0))
+
+        /* Pop and store snd elem address in pair address + 4 */
+        instrs :+ pop
+        instrs :+ Store(Register(8), Address(out, ImmInt(elemSize)))
+
+         /* Pop and store fst elem address in pair address */
+        instrs :+ pop
+        instrs :+ Store(Register(8), Address(out, ImmInt(0)))
+
+        val assembly = Assembly(out, instrs)
         return (assembly)
     }
 
