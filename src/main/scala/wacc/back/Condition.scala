0d52fb16f05710f2fa9e2a51646eb494ed065185
Rishi <rk1121@ic.ac.uk>
2023-02-28 14:56:01 +0000

    Add NO condition to indicate the opposite of AL

--- src/main/scala/wacc/back/Condition.scala
+++ src/main/scala/wacc/back/Condition.scala
@@ -5,8 +5,11 @@ object Condition extends Enumeration {
   type Condition = Value
   val EQ, NE, CS, CC, MI, PL, VS, VC, HI, LS, LT, GT, LE, GE = Value
   val AL: Value = Value("")
+  val NO: Value = Value("") // USED INTERNALLY ONLY
 
   def invert(cond: Condition): Condition = cond match {
+    case AL => NO
+    case NO => AL
     case EQ => NE
     case NE => EQ
     case LT => GE
