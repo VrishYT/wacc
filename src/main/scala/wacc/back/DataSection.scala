e6898685ab97a741490e89584c569aedb1df2c95
Preesha <pg721@ic.ac.uk>
2023-02-28 18:59:03 +0000

    Allow printing for pointers

--- src/main/scala/wacc/back/DataSection.scala
+++ src/main/scala/wacc/back/DataSection.scala
@@ -36,6 +36,7 @@ case object PrintCharSection extends PrintSection("c", "%c", "char")
 case object PrintIntSection extends PrintSection("i", "%d", "int")
 case object PrintStringSection extends PrintSection("s", "%.*s", "str") 
 case object PrintNewLine extends PrintSection("ln", "\\n", "ln")
+case object PrintPointerSection extends PrintSection("p", "%p", "p")
 
 case object PrintBoolSection extends DataSection {
     def toAssembly(): Seq[Instruction] = {
