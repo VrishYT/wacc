8d2a358a47b0a10cb88c9c83a750a806d5ebb7d3
Rishi <rk1121@ic.ac.uk>
2023-03-01 21:25:10 +0000

    Comment out unused MemoryAllocator unit tests

--- src/test/scala/wacc/back/MemoryAllocatorTest.scala
+++ src/test/scala/wacc/back/MemoryAllocatorTest.scala
@@ -3,37 +3,38 @@ package back
 
 import org.scalatest._
 
+@Ignore
 class MemoryAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
 
-  "A new memory allocator" should "have no variables" in {
-    val memory = new MemoryAllocator
-    assert(memory.memorySize == 0)
-  }
+  // "A new memory allocator" should "have no variables" in {
+  //   val memory = new MemoryAllocator
+  //   assert(memory.memorySize == 0)
+  // }
 
-  it should "be able to change in size" in {
-    val memory = new MemoryAllocator
-    memory.setSize(10)
-    assert(memory.memorySize == 10)
-  }
+  // it should "be able to change in size" in {
+  //   val memory = new MemoryAllocator
+  //   memory.setSize(10)
+  //   assert(memory.memorySize == 10)
+  // }
 
-  it should "allow variables to be stored" in {
-    Given("a new memory allocator of size 1, a register and a variable stored in that register")
-    val mem = new MemoryAllocator
-    mem.setSize(1)
-    val reg = Register(0)
-    val id = "default"
+  // it should "allow variables to be stored" in {
+  //   Given("a new memory allocator of size 1, a register and a variable stored in that register")
+  //   val mem = new MemoryAllocator
+  //   mem.setSize(1)
+  //   val reg = Register(0)
+  //   val id = "default"
 
-    When("a variable is stored from a register")
-    val instr = mem.store(id, reg)
+  //   When("a variable is stored from a register")
+  //   val instr = mem.store(id, reg)
 
-    Then("the memory allocator should have size 1")
-    assert(mem.memorySize === 1)
+  //   Then("the memory allocator should have size 1")
+  //   assert(mem.memorySize === 1)
 
-    And("it should return an instruction to move the value from the register into memory")
-    assert(instr == Store(reg, Address(FP, ImmInt(0))))
+  //   And("it should return an instruction to move the value from the register into memory")
+  //   assert(instr == Store(reg, Address(FP, ImmInt(0))))
 
-    And("we can query it for the new memory location of the variable")
-    val addr = mem.get(id)
-    assert(addr == Address(FP, ImmInt(0)))
-  }
+  //   And("we can query it for the new memory location of the variable")
+  //   val addr = mem.get(id)
+  //   assert(addr == Address(FP, ImmInt(0)))
+  // }
 }
