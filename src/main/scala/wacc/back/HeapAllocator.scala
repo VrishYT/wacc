5a583cb4a8ac6e8a0ff0f376a9a7c9e42f433db5
bc1221 <brett.conway21@imperial.ac.uk>
2023-03-02 15:57:09 +0000

    Debug nested PairElems by pushing Registers 1, 2 and 3 around Mallocs.
    Co-authored-by: Rishi Khiroya <rk1121@ic.ac.uk>

--- src/main/scala/wacc/back/HeapAllocator.scala
+++ src/main/scala/wacc/back/HeapAllocator.scala
@@ -5,7 +5,11 @@ import scala.collection.mutable.ListBuffer
 
 class HeapAllocator {
 
-    val malloc = LinkBranch("malloc")
+    val malloc = Seq(
+        Push(Register(1), Register(2), Register(3)),
+        LinkBranch("malloc"),
+        Pop(Register(1), Register(2), Register(3))
+    )
     val pop = Pop(Register(8))
     val push = Push(Register(8))
     val elemSize = 4
@@ -13,7 +17,7 @@ class HeapAllocator {
      def mallocPair(fst: Operand, snd: Operand, out: Register): Assembly = {
 
         def mallocPairElem(elem: Operand): Seq[Instruction] = {
-            val instrs = Seq(Mov(Register(0), ImmInt(elemSize)), malloc, Mov(out, Register(0)),  Mov(Register(8), elem))
+            val instrs = (Mov(Register(0), ImmInt(elemSize)) +: malloc) ++ Seq(Mov(out, Register(0)),  Mov(Register(8), elem))
 
             val rest = elem match {
                 case ImmChar(_) => Seq(StoreB(Register(8), Address(out, ImmInt(0))), Mov(Register(8), out), push)
@@ -24,8 +28,8 @@ class HeapAllocator {
         }
 
         val instrs = Seq(push) ++ mallocPairElem(fst) ++ mallocPairElem(snd) ++ Seq(Mov(Register(0), 
-                                                                                    ImmInt(elemSize * 2)), 
-                                                                                    malloc,
+                                                                                    ImmInt(elemSize * 2))) ++ 
+                                                                                    malloc ++ Seq(
                                                                                      Mov(out, Register(0)),
                                                                                     pop, 
                                                                                     Store(Register(8), Address(out, ImmInt(elemSize))),
@@ -44,7 +48,7 @@ class HeapAllocator {
         /* Malloc for the size of array */
         instrs += push
         instrs += Mov(Register(0), ImmInt(elemSize * (numElems + 1)))
-        instrs += malloc
+        instrs ++= malloc
 
         /* Set out to malloced address + elemSize */
         instrs += Mov(out, Register(0))
