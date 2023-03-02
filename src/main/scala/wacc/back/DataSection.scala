f9ecd98680f29ec17803efc5cdb3cd4e1f4f9cd1
Rishi <rk1121@ic.ac.uk>
2023-03-02 03:56:30 +0000

    Move TextSection into its own file. Modify TextSection to allow double quotes in string literals.

--- src/main/scala/wacc/back/DataSection.scala
+++ src/main/scala/wacc/back/DataSection.scala
@@ -1,7 +1,6 @@
 package wacc
 package back
 
-import scala.collection.mutable.{Map => MapM}
 import wacc.back._
 import Condition._
 import ast.Func
@@ -143,42 +142,6 @@ case object FreePairSection extends DataSection {
             LinkBranch("free"),
             Pop(PC, Register(8))
         )
-    }
-}
-
-class TextSection extends DataSection {
-
-    private val table = MapM[String, String]()
-    private var counter = 0
-
-    def add(value: String): String = {
-        val label = ".L.str" + counter
-        table(label) = value
-        counter += 1
-        return label
-    }
-
-    def get(id: String): String = table.get(id) match {
-        case Some(x) => x
-        case None => ???
-    }
-
-    override def toAssembly(): Seq[Instruction] = {
-        if (table.isEmpty) return Seq()
-
-        def entryToAssembly(entry: (String, String)): Seq[Instruction] = {
-            val label = entry._1
-            val data = entry._2
-
-            return Seq(
-                Directive(s".word ${data.length}"),
-                Label(label),
-                Directive(s".asciz \"${data}\"")
-            )
-        }
-
-        return Section(".data") +: table.map(entryToAssembly).fold(Seq())(_ ++ _) :+ Section(".text")
-
     }
 }
 
