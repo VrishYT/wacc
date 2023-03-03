959b579ad2efde35f7e327ce969551fa6bb1d9ac
bc1221 <brett.conway21@imperial.ac.uk>
2023-03-03 16:46:00 +0000

    Implement Unit tests for RegisterAllocatorTest.
    Add unit test for save function.
    Add unit test for restore function.

--- src/test/scala/wacc/back/RegisterAllocatorTest.scala
+++ src/test/scala/wacc/back/RegisterAllocatorTest.scala
@@ -6,10 +6,12 @@ import org.scalatest._
 
 class RegisterAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
 
+/*
     val fs = (IntType, "getX")
     val args = List()
     val stats = List(Declare(IntType, "x", IntLiteral(9), Return(Ident("x"))))
     val modelAst = Program(List(Func(fs, args, stats)),  List(Skip))
+    */
 
   "A new register allocator" should "have no allocated registers for r1 to r10" in {
 
@@ -21,6 +23,34 @@ class RegisterAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
     }
   }
 
+   "A new register allocator" should "be able to save a list of registers" in {
+
+    val memory = new MemoryAllocator
+    val regAllocator = new RegisterAllocator(memory)
+
+    val reg1 = Register(1)
+    val reg2 = Register(2)
+    val reg3 = Register(3)
+
+    val instr = regAllocator.save(reg1, reg2, reg3)
+
+    assert(instr == Push(reg1, reg2, reg3))
+  }
+
+     "A new register allocator" should "be able to restore a list of registers" in {
+
+    val memory = new MemoryAllocator
+    val regAllocator = new RegisterAllocator(memory)
+
+    val reg1 = Register(6)
+    val reg2 = Register(7)
+    val reg3 = Register(8)
+    val reg4 = Register(10)
+
+    val instr = regAllocator.save(reg1, reg2, reg3, reg4)
+
+    assert(instr == Push(reg1, reg2, reg3, reg4))
+  }
 
 /*
   "A new register allocator" should "have no allocated registers for r1 to r10" in {
