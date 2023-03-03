c1b2718f80519805a9dc92bbaae5a683e617c8c9
Preesha <pg721@ic.ac.uk>
2023-03-03 14:01:48 +0000

    Improve style and document Operand Test amd MemoryAllocatorTest.
    Improve style of OperandTests.scala.
    Add documentation for MemoryAllocatorTests.scala.
    
    Co-authored-by: Brett Conway <bc1221@ic.ac.uk>

--- src/test/scala/wacc/back/MemoryAllocatorTest.scala
+++ src/test/scala/wacc/back/MemoryAllocatorTest.scala
@@ -6,135 +6,199 @@ import org.scalatest._
 class MemoryAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
 
   "A new memory allocator" should "have no variables" in {
+
     val memory = new MemoryAllocator
     val c = memory.getCount
     assert(c == 0)
+
   }
 
   "A grow instruction" should "return a valid Instruction object for a stack grow instruction" in {
+
     val memory = new MemoryAllocator
     val instr = memory.grow(7)
     assert(instr == Sub(SP, SP, ImmInt(28)))
+
   }
 
   "A shrink instruction" should "return a valid Instruction object for a stack shrink instruction" in {
+
     val memory = new MemoryAllocator
     val instr = memory.shrink(4)
     assert(instr == Add(SP, SP, ImmInt(16)))
+
   }
 
 
   "A grow instruction" should "return a string for an assembly stack grow instruction" in {
+
     val memory = new MemoryAllocator
     val sb = new StringBuilder
     val instr = memory.grow(2).arm11(sb)
+
     assert(sb.toString == "\tsubs sp, sp, #8\n")
+
   }
 
   "A shrink instruction" should "return a string for an assembly stack shrink instruction" in {
+
     val memory = new MemoryAllocator
     val sb = new StringBuilder
     val instr = memory.shrink(3).arm11(sb)
+
     assert(sb.toString == "\tadds sp, sp, #12\n")
+
   }
 
   "A memory allocator" should "increase its count by 1 when stored to" in {
+
     val memory = new MemoryAllocator
     assert(memory.getCount == 0)
+
     val assembly1 = memory.store("a_variable", Register(4))
     assert(memory.getCount == 1)
+
     val assembly2 = memory.store("another_variable", Register(5))
     assert(memory.getCount == 2)
+
   }
 
   "A memory allocator" should "return a valid Assembly object for a store instruction" in {
+
     val memory = new MemoryAllocator
 
+    /* Get count before store is called */
     val currCount = memory.getCount
+
     val reg = Register(1)
 
     val assembly = memory.store("a_variable", reg)
 
+    /* Identify the instructions and operand in the assembly returned by store */
     val instrs = assembly.instr
     val op = assembly.getOp
 
+    /* Check that store returned the expected Opcode */
     assert(op == Address(FP, ImmInt(-currCount * 4)))
+    /* Check that there is only one instruction in the sequence returned by store */
     assert(instrs.size == 1)
 
+    /* Retrieve the one instruction returned by store into instr */
     val instr = instrs.head
+
+    /* Check that the instruction returned by store is a Store Instruction from the
+       stored register into the frame point -count * 4, (where count is the allocator's
+       count before the store). */
     assert(instr == Store(reg, Address(FP, ImmInt(-currCount * 4))))
+
   }
 
   "A memory allocator" should "return a valid Assembly object for a store instruction after previous stores" in {
+
     val memory = new MemoryAllocator
 
+    /* Perform 3 stores on different variables */
     memory.store("a_variable1", Register(5))
     memory.store("a_variable2", Register(6))
     memory.store("a_variable3", Register(2))
 
+    /* Get count before next store is called */
     val currCount = memory.getCount
 
     val reg = Register(7)
 
     val assembly = memory.store("a_variable", reg)
 
+    /* Identify the instructions and operand in the assembly returned by the store being
+       observed */
     val instrs = assembly.instr
     val op = assembly.getOp
 
+    /* Check that store returned the expected Opcode */
     assert(op == Address(FP, ImmInt(-currCount * 4)))
+    /* Check that there is only one instruction in the sequence returned by store */
     assert(instrs.size == 1)
 
+    /* Retrieve the one instruction returned by store into instr */
     val instr = instrs.head
+    /* Check that the instruction returned by the store is a Store Instruction from the
+       stored register into the frame point -count * 4, (where count is the allocator's
+       count before the store). */
     assert(instr == Store(reg, Address(FP, ImmInt(-currCount * 4))))
+
   }
 
   "A memory allocator" should "return a valid string for a store instruction" in {
+
     val memory = new MemoryAllocator
 
     val reg = Register(1)
 
     val assembly = memory.store("a_variable", reg)
 
+    /* Identify the instructions and operand in the assembly returned by the store being
+       observed */
     val instrs = assembly.instr
     val op = assembly.getOp
 
+    /* Check that the opcode returned by the store provides the expected string for 
+       the initial count (0) */
     assert(op.toString == "[fp,#0]")
 
+    /* Check that there is only one instruction in the sequence returned by store */
     assert(instrs.size == 1)
+    /* Retrieve the one instruction returned by store into instr */
     val instr = instrs.head
 
+    /* String build the arm11 string of the instruction */
     val sb = new StringBuilder
     instr.arm11(sb)
 
+    /* Check that the string built arm11 instruction is the expected string for the 
+       given register and initial count */
     assert(sb.toString == "\tstr r1, [fp,#0]\n")
   }
 
   "A memory allocator" should "return a valid string for a store instruction, after multiple stores" in {
+
     val memory = new MemoryAllocator
 
+    /* Perform 4 stores on different variables */
     memory.store("a_variable1", Register(5))
     memory.store("a_variable2", Register(6))
     memory.store("a_variable3", Register(2))
     memory.store("a_variable4", Register(6))
 
+    /* Get count before next store is called */
     val currCount = memory.getCount
+    /* Calculate the expected ImmInt value of the returned address from the next store */
     val imm = -currCount * 4
 
     val reg = Register(1)
 
     val assembly = memory.store("a_variable", reg)
 
+    /* Identify the instructions and operand in the assembly returned by the store being
+      observed */
     val instrs = assembly.instr
     val op = assembly.getOp
 
+    /* Check the Operand returned by the store has the expected string value */
     assert(op.toString == "[fp,#"+imm+"]")
 
+    /* Check that there is only one instruction in the sequence returned by store */
     assert(instrs.size == 1)
+    /* Retrieve the one instruction returned by store into instr */
     val instr = instrs.head
 
+    /* String build the arm11 string of the instruction */
     val sb = new StringBuilder
     instr.arm11(sb)
 
+    /* Check that the string built arm11 instruction is the expected string for the 
+       given register and appropriate count (the one recorded just before the store
+       being observed by this test) */
     assert(sb.toString == "\tstr r1, [fp,#"+imm+"]\n")
+
   }
 }
--- src/test/scala/wacc/back/OperandTest.scala
+++ src/test/scala/wacc/back/OperandTest.scala
@@ -7,33 +7,45 @@ import org.scalatest.matchers.should.Matchers
 class OperandTests extends AnyFlatSpec with Matchers {
     
     "ImmInt operand" should "return a valid string representation of immediate integers" in {
+
         val i = ImmInt(6).toString
         i should equal ("#6") 
+
     }
 
     "ImmChar operand" should "return a valid string representation of immediate characters" in {
+
         val i = ImmChar('a').toString
         i should equal ("#97") 
+
     }
 
     "DataLabel operand" should "return a valid string representation of labels" in {
+
         val i = DataLabel("L.str.0").toString
         i should equal ("=L.str.0") 
+
     }
 
     "Address operand" should "return a valid string representation of addresses" in {
+
         val i =Address(Register(0), ImmInt(5)).toString
         i should equal ("[r0,#5]") 
+
     }
 
     "ASR operand" should "return a valid string representation of the instruction ASR" in {
+
         val i = ASR(Register(3), ImmInt(90)).toString
         i should equal ("r3, asr #90") 
+
     }
 
     "LSL operand" should "return a valid string representation of the instruction LSL" in {
+
         val i = LSL(Register(7), Register(2)).toString
-        i should equal ("r7, lsl r2") 
+        i should equal ("r7, lsl r2")
+         
     }
 
 
