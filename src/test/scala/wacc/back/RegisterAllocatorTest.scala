dd966330d9262d0349fff59986bf754cec5de762
bc1221 <brett.conway21@imperial.ac.uk>
2023-03-03 16:56:25 +0000

    Remove model ast from RegisterAllocatorTests

--- src/test/scala/wacc/back/RegisterAllocatorTest.scala
+++ src/test/scala/wacc/back/RegisterAllocatorTest.scala
@@ -6,13 +6,6 @@ import org.scalatest._
 
 class RegisterAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
 
-/*
-    val fs = (IntType, "getX")
-    val args = List()
-    val stats = List(Declare(IntType, "x", IntLiteral(9), Return(Ident("x"))))
-    val modelAst = Program(List(Func(fs, args, stats)),  List(Skip))
-    */
-
   "A new register allocator" should "have no allocated registers for r1 to r10" in {
 
     val memory = new MemoryAllocator
@@ -23,7 +16,7 @@ class RegisterAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
     }
   }
 
-   "A new register allocator" should "be able to save a list of registers" in {
+  "A register allocator" should "be able to save a list of registers" in {
 
     val memory = new MemoryAllocator
     val regAllocator = new RegisterAllocator(memory)
@@ -37,7 +30,7 @@ class RegisterAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
     assert(instr == Push(reg1, reg2, reg3))
   }
 
-     "A new register allocator" should "be able to restore a list of registers" in {
+  "A register allocator" should "be able to restore a list of registers" in {
 
     val memory = new MemoryAllocator
     val regAllocator = new RegisterAllocator(memory)
@@ -51,24 +44,18 @@ class RegisterAllocatorTests extends flatspec.AnyFlatSpec with GivenWhenThen {
 
     assert(instr == Push(reg1, reg2, reg3, reg4))
   }
-
 /*
-  "A new register allocator" should "have no allocated registers for r1 to r10" in {
+  "A register allocator" should "should be able to allocate a register" in {
 
     val memory = new MemoryAllocator
     val regAllocator = new RegisterAllocator(memory)
-    val symbolTable = new SymbolTable
-
-    def getFuncTable(id: String): FuncTable = symbolTable.get(id) match {
-            case Some(x) => x
-            case None => ???
-     }
+    val table = new SymbolTable
 
     val init_size = regAllocator.freeRegs.size
 
-    val regAssemb = regAllocator.allocate
+    val regAssemb = regAllocator.allocate(table)
 
     assert(regAllocator.freeRegs.size == init_size - 1)
   }
-  */
+*/
 }
