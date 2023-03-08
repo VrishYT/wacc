9b9eefba17c4fcf75613655cacaf199534300b40
Rishi <rk1121@ic.ac.uk>
2023-03-08 15:13:26 +0000

    Modify Annotation to contain an UnknownAnnotation object used for parser error handling. Add verifyBranch to Annotation for semantic checking.

--- src/main/scala/wacc/ast/Annotation.scala
+++ src/main/scala/wacc/ast/Annotation.scala
@@ -5,15 +5,43 @@ package ast
 
 import parsley.genericbridges.ParserBridge1
 
-sealed abstract class Annotation
+sealed abstract class Annotation {
+    def isValid: Boolean = true
+    def verify(func: Func): Boolean = false
+}
 
 object Annotation extends ParserBridge1[String, Annotation] {
     def apply(id: String): Annotation = id match {
         case "tailrec" => TailRecursiveAnnotation
-        case _ => ???
+        case _ => UnknownAnnotation
     }
 }
 
-case object TailRecursiveAnnotation extends Annotation
+case object TailRecursiveAnnotation extends Annotation {
+    override def verify(func: Func): Boolean = {
+        def verifyBranch(stats: List[Stat]): Boolean = {
+            var lastVar = ""
+            stats.foreach(stat => stat match {
+                case If(_, x, y) => {
+                    val thenBranch = verifyBranch(x)
+                    val elseBranch = verifyBranch(y)
+
+                    // TODO: OR/AND
+
+                }
+                case Begin(xs) => {
+                    val branch = verifyBranch(xs)
+                }
+                case _ => 
+            })
+            true
+        }
+
+        verifyBranch(func.stats)
+    }
+}
+case object UnknownAnnotation extends Annotation {
+    override def isValid: Boolean = false
+}
 
 
