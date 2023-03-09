093e6558fafb82df3bb9665f44a2c4d2c29ff54e
Rishi <rk1121@ic.ac.uk>
2023-03-09 12:07:06 +0000

    Modify TailRecursiveAnnotation.verify to handle child scoping.

--- src/main/scala/wacc/ast/Annotation.scala
+++ src/main/scala/wacc/ast/Annotation.scala
@@ -20,21 +20,32 @@ object Annotation extends ParserBridge1[String, Annotation] {
 case object TailRecursiveAnnotation extends Annotation("Function is not tail-recursive\n - cannot optimize with \'@tailrec\' annotation") {
     override def verify(func: Func): Boolean = {
         def verifyBranch(stats: List[Stat]): Boolean = {
-            var lastVar = ""
+            var tailCallVar: Any = null
             stats.foreach(stat => stat match {
+                case Declare(_, id, rhs) => rhs match {
+                    case Call(funcId, _) if (funcId == func.fs._2) => tailCallVar = Ident(id)(0,0) 
+                    case _ => 
+                }
+                case Assign(lval, rval) => lval match {
+                    case x: LExpr => rval match {
+                        case Call(funcId, _) if (funcId == func.fs._2) => tailCallVar = x
+                        case _ => 
+                    }
+                }
                 case If(_, x, y) => {
                     val thenBranch = verifyBranch(x)
                     val elseBranch = verifyBranch(y)
-
-                    // TODO: OR/AND
-
+                    return thenBranch || elseBranch
                 }
                 case Begin(xs) => {
                     val branch = verifyBranch(xs)
                 }
+                case Return(expr) => {
+                    return tailCallVar == expr
+                }
                 case _ => 
             })
-            true
+            false
         }
 
         verifyBranch(func.stats)
