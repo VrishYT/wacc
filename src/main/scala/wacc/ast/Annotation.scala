a23f6fa835dc4982ab3aebdcfba156aeecd9188a
Rishi <rk1121@ic.ac.uk>
2023-03-14 05:36:06 +0000

    Modify TailRecursiveAnnotation.process() to replace tail recursive calls with re-assigns and a continue statement.

--- src/main/scala/wacc/ast/Annotation.scala
+++ src/main/scala/wacc/ast/Annotation.scala
@@ -36,7 +36,10 @@ case object TailRecursiveAnnotation extends Annotation("Function is not tail-rec
                     case _ => 
                 }
                 case AssignOrTypelessDeclare(lval, rval) => rval match {
-                    case Call(ids, _) if (matchId(ids, func.fs._2)) => tailCallVar = lval
+                    case Call(ids, _) if (matchId(ids, func.fs._2)) => lval match {
+                        case x: Ident => tailCallVar = x
+                        case _ => 
+                    }
                     case _ => 
                 }
                 case If(_, x, y) => {
@@ -69,162 +72,143 @@ case object TailRecursiveAnnotation extends Annotation("Function is not tail-rec
 
         // TODO
 
-        val instr = ListBuffer[Stat]()
+        def processStats(stats: List[Stat])(implicit table: MapM[String, TableEntry], child: Table): List[Stat] = {
 
-        def processStats(stats: List[Stat], cond: Option[(Expr, Boolean)] = None)(implicit table: MapM[String, TableEntry], child: Table): Unit = {
             var ifCount = 0
             var whileCount = 0
             var beginCount = 0
 
-            val modifiedVars = ListBuffer[LValue]()
-            val conditions = ListBuffer[(Expr, Boolean)]()
+            val out = ListBuffer[Stat]()
 
-            def addConditional(out: Stat, cond: Expr, inverted: Boolean): Stat = {
-                child.addIf(ChildTable(child), ChildTable(child))
-                if (inverted) {
-                    If(cond, List(Skip), List(out))
-                } else {
-                    If(cond, List(out), List(Skip))
-                }
-
+            def isModified(id: Ident): Boolean = child.getSymbol(id.id) match {
+                case Some(x) => x.modified
+                case _ => ???
             }
 
-            def modifyVars(lval: LValue, call: Call): Unit = {
+            def modifyVars(id: Ident, call: Call): Unit = {
                 val callArgs = call.args
                 val funcArgs = func.args.map(_.id).filter(_ != "this")
-                modifiedVars += lval
-                funcArgs.zip(callArgs).foreach(x => {
-                    var out: Stat = AssignOrTypelessDeclare(Ident(x._1)(0,0), x._2)
-                    out = cond match {
-                        case Some(x) => x match {
-                            case (x: Expr, inverted: Boolean) => addConditional(out, x, inverted)
-                            case _ => ???
-                        }
-                        case _ => out
+                child.getSymbol(id.id) match {
+                    case Some(x) => x.modified = true
+                    case _ => {
+                        println(s"id = $id")
+                        println(child)
+                        ???
                     }
-                    instr += out
-                })
-
+                }
+                funcArgs.zip(callArgs).foreach(x => out += AssignOrTypelessDeclare(Ident(x._1)(0,0), x._2))
             }
 
             stats.foreach (stat => stat match {
                 case Declare(_, id, rhs) => rhs match {
-                    // TODO: if a call, re-assign params instead of passing in
                     case call@Call(ids, _) if (ids.last == func.fs._2) => {
-                        cond match {
-                            case Some(x) => {
-                                conditions += x
-                                println(conditions)
-                            }
-                            case _ => 
-                        }
                         modifyVars(Ident(id)(0,0), call)
                     }
-                    case _ => instr += stat
+                    case _ => out += stat
                 }
                 case AssignOrTypelessDeclare(lval, rval) => lval match {
                     case x: LValue => rval match {
-                        case call@Call(ids, _) if (ids.last == func.fs._2) => {
-                            cond match {
-                                case Some(x) => {
-                                    conditions += x
-                                    println(conditions)
-                                }
-                                case _ => 
-                            }
-                            modifyVars(x, call)
+                        case call@Call(ids, _) if (ids.last == func.fs._2) => lval match {
+                            case x: Ident => modifyVars(x, call)
+                            case _ => ???
                         }
-                        case _ => instr += stat
+                        case _ => out += stat
                     }
                 }
                 case If(cond, x, y) => {
-                    // TODO
-                    val thenTable = table.get(s"_if${ifCount}") match {
+                    val thenChild = child.getTable(s"_if${ifCount}") match {
+                        case Some(x) => x
+                        case None => ???
+                    }
+                    val thenCloned = table.get(s"_if${ifCount}") match {
                         case Some(x) => x match {
                             case x: Table => x.table
                             case _ => ???
                         }
                         case None => ???
                     } 
-                    // TODO
-                    processStats(x, Some(cond, false))(thenTable, child)
-                    val elseTable = table.get(s"_else${ifCount}") match {
+                    val thenStats = processStats(x)(thenCloned, thenChild)
+                    val elseChild = child.getTable(s"_else${ifCount}") match {
+                        case Some(x) => x
+                        case None => ???
+                    }
+                    val elseCloned = table.get(s"_else${ifCount}") match {
                         case Some(x) => x match {
                             case x: Table => x.table
                             case _ => ???
                         }
                         case None => ???
                     }
-                    processStats(y, Some(cond, true))(elseTable, child)
-                    // ifCount += 1
+                    val elseStats = processStats(y)(elseCloned, elseChild)
+                    ifCount += 1
+                    out += If(cond, thenStats, elseStats)
                 }
-                case While(_, xs) => {
-                    // TODO
-                    val childTable = table.get(s"_while${whileCount}") match {
+                case While(cond, xs) => {
+                    val childTable = child.getTable(s"_while${whileCount}") match {
+                        case Some(x) => x
+                        case None => ???
+                    }
+                    val cloned = table.get(s"_while${whileCount}") match {
                         case Some(x) => x match {
                             case x: Table => x.table
                             case _ => ???
                         }
                         case None => ???
                     }
-                    processStats(xs)(childTable, child)
-                    // whileCount += 1
+                    val newStats = processStats(xs)(cloned, childTable)
+                    whileCount += 1
+                    out += While(cond, newStats)
                 }
                 case Begin(xs) => {
-                    // TODO
-                    val childTable = table.get(s"_begin${beginCount}") match {
+                    val childTable = child.getTable(s"_begin${beginCount}") match {
+                        case Some(x) => x
+                        case None => ???
+                    }
+                    val cloned = table.get(s"_begin${beginCount}") match {
                         case Some(x) => x match {
                             case x: Table => x.table
                             case _ => ???
                         }
                         case None => ???
                     }
-                    processStats(xs)(childTable, child)
-                    // beginCount += 1
+                    beginCount += 1
+                    out += Begin(processStats(xs)(cloned, childTable))
                 }
                 case Return(expr) => expr match {
-                    case x: LValue if modifiedVars.contains(x) =>
-                    case _ => cond match {
-                        case Some(x) => {
-                            conditions += x
-                            println(conditions)
-                            instr += addConditional(stat, x._1, x._2)
-                        }
-                        // {
-                        //     val out = if (x._2) If(x._1, List(Skip), List(stat)) else If(x._1, List(stat), List(Skip))
-                        //     table.ifCount += 1
-                        //     instr += out
-                        // }
-                        case None => instr += stat
-                    }
+                    case x: Ident if isModified(x) => out += Continue(0,0)
+                    case _ => out += stat
                 }
-                case _ => cond match {
-                    case Some(x) => instr += addConditional(stat, x._1, x._2)
-                    case None => instr += stat
-                }
+                case _ => out += stat
             })
-
-            // println(s"modified = $modifiedVars")
-        }
-
+            out.toList
+        }        
+        
+        println(funcTable)
         val table = funcTable.table.clone()
-        // println(funcTable)
-        funcTable.table.filterInPlace((k, v) => v match {
-            case _: Symbol => true
-            case _ => false
-        })
+        funcTable.table.clear()
         val whileTable = ChildTable(funcTable)
-        processStats(func.stats)(table, whileTable)
+        whileTable.table ++= table.map(entry => entry._2 match {
+            case x: ChildTable => {
+                val clone = ChildTable(whileTable)
+                clone.table ++= x.table
+                (entry._1, clone)
+            }
+            case _ => entry
+        })
         funcTable.addWhile(whileTable)
+        val stats = processStats(func.stats)(table, whileTable)
+        
+        // println(whileTable)
+        // println(funcTable)
 
-        // println(instr)
+        // println(stats)
 
         TypedFunc(
             func.annotations, 
             func.isPrivate, 
             func.fs,
             func.args.toList,
-            List(While(BoolLiteral(true)(0,0), instr.toList))
+            List(While(BoolLiteral(true)(0,0), stats))
         )(func.pos)
     }
 
