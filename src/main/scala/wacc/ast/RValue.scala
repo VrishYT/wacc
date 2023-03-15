c68bce3bcb80883571e80b763f81defbe45eaf32
Rishi <rk1121@ic.ac.uk>
2023-03-15 15:29:41 +0000

    Modify Call to evaluate boolean expression arguments before passing into the function.

--- src/main/scala/wacc/ast/RValue.scala
+++ src/main/scala/wacc/ast/RValue.scala
@@ -130,7 +130,8 @@ case class Call(var id: List[String], args: List[Expr])(val pos: (Int, Int)) ext
             }
             return callAssembly(id, gen, list, classSymbol.op, table)
         } else {
-            val out = args.map(_.toAssembly(gen))
+            val out = args.map(_.toAssembly(gen).condToReg(gen.regs))
+            // println(s"call ${id.mkString(".")}(${args.mkString(",")})")
             val func = Func.callFunction(s"wacc_${id.head}", args = out.map(_.getOp()), gen = gen)
             return Assembly(Register(0), out.map(_.instr).fold(Seq())(_ ++ _) ++ func)
         }
